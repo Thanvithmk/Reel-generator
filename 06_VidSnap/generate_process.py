d08d0808ae90752b7fe8bdb11a771912253eaddc
Thanvith Manjunath K <thanvithmk@gmail.com>
2025-11-10 23:08:42 +0530

    audio generated and integrated text-to-audio functionality

--- 06_VidSnap/generate_process.py
+++ 06_VidSnap/generate_process.py
@@ -1,20 +1,24 @@
-import os
+import os,time
+from text_to_audio import text_to_speech_file
 
 def text_to_audio(folder):
-    pass
-
+    with open(f"/Users/thanvithmk/git_clones/Reel-generator/Reel-generator/06_VidSnap/user_uploads/{folder}/desc.txt") as f:
+        text=f.read()
+    text_to_speech_file(text,folder)
+    
 def create_reel(folder):
     pass
 
 if __name__=="__main__":
-    with open("done.txt","r") as f:
-        done_folders=f.readlines()
-    done_folders=[x.strip() for x in done_folders]
-    folders=os.listdir("/Users/thanvithmk/git_clones/Reel-generator/Reel-generator/06_VidSnap/user_uploads")
-    for folder in folders:
-        if (folder not in done_folders):
-            text_to_audio(folder)
-            create_reel(folder)
-            with open("done.txt","a") as f:
-                f.write(folder+"\n")
-
+        while True:
+            with open("done.txt","r") as f:
+                done_folders=f.readlines()
+            done_folders=[x.strip() for x in done_folders]
+            folders=os.listdir("/Users/thanvithmk/git_clones/Reel-generator/Reel-generator/06_VidSnap/user_uploads")
+            for folder in folders:
+                if (folder not in done_folders):
+                    text_to_audio(folder)
+                    create_reel(folder)
+                    with open("done.txt","a") as f:
+                        f.write(folder+"\n")
+            time.sleep(5)
